43d8929f80672f4c34496d8e12728c9103b652f6
drewmw5 <drewmwilliams55@gmail.com>
2022-11-23 22:52:24 -0800

    renamed vite.config.js to .ts
    Added server object to get HMR working

--- stubs/inertia-react-ts/vite.config.ts
+++ stubs/inertia-react-ts/vite.config.ts
@@ -2,12 +2,24 @@ import { defineConfig } from 'vite';
 import laravel from 'laravel-vite-plugin';
 import react from '@vitejs/plugin-react';
 
+const certs = {
+    cert: "../certs/localhost.crt",
+    key: "../certs/localhost.key",
+}
+
 export default defineConfig({
     plugins: [
         laravel({
-            input: 'resources/js/app.jsx',
+            input: 'resources/js/app.tsx',
             refresh: true,
         }),
         react(),
     ],
+    server: {
+        host: true,
+        https: certs,
+        hmr: {
+            host: "localhost",
+        }
+    }
 });
